91fac0b0cc82ed601a435cae613a04ea98f8b23c
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-08-08 10:30:49 -0700

    partial solution for recursion problem, not completed

--- recursion-divs/src/problem.js
+++ recursion-divs/src/problem.js
@@ -2,4 +2,12 @@
 // `div` elements, and false if the element contains fewer than five.
 var containsFiveOrMoreDivs = function(domElement) {
   // Your code here.
+console.dir(domElement);
+  var checker = function() {
+
+  }
+  console.log(domElement);
 };
+
+
+/* need to run recursion, and could use closure as one method to solve */
